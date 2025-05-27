06f3057c22a4d31459d16d02d5b47e0a85c84d96
AxelErichsen <axelbe@live.dk>
2025-05-28 01:41:36 +0200

    docker compose/file

--- Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9140cd2dee704fc532acb10217e4b543ec17ed86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8f3586f827d8f38b173fb998db4731e18b327f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
