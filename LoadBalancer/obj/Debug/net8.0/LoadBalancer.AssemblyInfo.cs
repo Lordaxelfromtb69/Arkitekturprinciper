8661e76896f5dd4b02ed0fce8cada01446dc617f
AxelErichsen <axelbe@live.dk>
2025-05-27 18:09:30 +0200

    første commit

--- LoadBalancer/obj/Debug/net8.0/LoadBalancer.AssemblyInfo.cs
+++ LoadBalancer/obj/Debug/net8.0/LoadBalancer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoadBalancer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6dfb00a42a55a12a194def4beb96e4df81cefe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb73e809a02ec98562bd4eaeb79388f02d84a03e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoadBalancer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoadBalancer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6dfb00a42a55a12a194def4beb96e4df81cefe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb73e809a02ec98562bd4eaeb79388f02d84a03e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
