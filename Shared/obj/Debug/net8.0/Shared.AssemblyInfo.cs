c670564651465ba2178ee3e0213f957f67e3407d
AxelErichsen <axelbe@live.dk>
2025-05-27 23:37:44 +0200

    a

--- Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204e1079f644a0bf36c4f4761aeff7ddbb9f0fc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d23541bac1554c9a4d73514579a3a23c9d5865")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
